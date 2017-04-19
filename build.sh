6074f17788b755fabfdf5de876ed8e728d84729a
bananaappletw <bananaappletw@gmail.com>
2017-04-19 16:48:15 +0800

    Add gdb ignore

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 GDB_VERSION='7.12'
 wget https://ftp.gnu.org/gnu/gdb/gdb-$GDB_VERSION.tar.gz
 tar zxvf gdb-$GDB_VERSION.tar.gz
-cd gdb-$GDB_VERSION
+mv gdb-$GDB_VERSION/ gdb/
+cd gdb/
 ./configure
 make -j $(grep processor < /proc/cpuinfo | wc -l)
 sudo cp gdb/gdb /usr/bin/gdb
