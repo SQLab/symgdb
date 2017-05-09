bf4f3b3b36ca639c8dfcc65d30405221dc2c2e29
bananaappletw <bananaappletw@gmail.com>
2017-05-09 23:23:41 +0800

    Remove .gdb_history and simplify code

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR=$(dirname "$(readlink -f "$0")")
-gdb --batch -x $DIR/crackme_hash_32 $DIR/../examples/crackme_hash_32
+gdb -x $DIR/crackme_hash_32 $DIR/../examples/crackme_hash_32
 gdb -x $DIR/crackme_hash_64 $DIR/../examples/crackme_hash_64
