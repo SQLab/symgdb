42a8263141eb46263c6bdfaaa9e0a8a6a15688e6
bananaappletw <bananaappletw@gmail.com>
2017-05-11 15:53:57 +0800

    Add comment about singleton

--- singleton.py
+++ singleton.py
@@ -1,7 +1,12 @@
 class Singleton(object):
+    """
+    To be compatible with peda, have to use class way of singleton.
+    Neither decorator nor metaclass.
+    """
     _instance = None
     _initialized = False
-    def __new__(cls,*args, **kwargs):
+
+    def __new__(cls, *args, **kwargs):
         if cls._instance is None:
-            cls._instance = super(Singleton,cls).__new__(cls, *args, **kwargs)
+            cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
         return cls._instance
